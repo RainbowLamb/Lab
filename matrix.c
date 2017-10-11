7b74e6b5d6c960a98d630c83252b8da12d3b137a
Reznikova Varia <31695448+RainbowLamb@users.noreply.github.com>
2017-10-11 21:56:03 +0300

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -14,7 +14,6 @@ int main()
 	int i,j;
     if (m>=3, n>=4);
     {
-    
         for (i=0; i<m; i++)
         {
             for (j=0; j<n; j++)
@@ -34,33 +33,26 @@ int main()
         massive[0][j] = massive[m - 1][j];
         massive[m - 1][j] = ch;
     }
-
     for ( i = 0; i < n/2; i++) {
         int ch;
         ch = massive[0][i];
         massive[0][i] = massive[0][n - 1 - i];
         massive[0][n - 1 - i] = ch;
-
+	    
         ch = massive[m - 1][i];
         massive[m - 1][i] = massive[m - 1][n - 1 - i];
         massive[m - 1][n - 1 - i] = ch;
-
     }
 
 
 
     printf("\n   \n");
-
     for ( i = 0; i < m; i++) {
         for (j = 0; j < n; j++){
             printf("%d\t ",massive[i][j]);
         }
         printf("\n");
     }
-
-
     getchar();
     return 0;
-
-
 }
