35fb9ed333c6b1cb2c121950bf9d1ab86ee71d2a
Reznikova Varia <31695448+RainbowLamb@users.noreply.github.com>
2017-10-11 22:23:59 +0300

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -14,6 +14,7 @@ int main()
 	int i,j;
     if (m>=3, n>=4);
     {
+    printf ("\n Massive\n\n");
         for (i=0; i<m; i++)
         {
             for (j=0; j<n; j++)
@@ -28,31 +29,64 @@ int main()
         printf("Error");
     
     /* part 2 */
-      for( j = 0; j < n; j++) {
+    printf ("\n Switching\n");
+      for( j = 0; j < n; j++) 
+      {
         int ch = massive[0][j];
         massive[0][j] = massive[m - 1][j];
         massive[m - 1][j] = ch;
     }
+
     for ( i = 0; i < n/2; i++) {
         int ch;
         ch = massive[0][i];
         massive[0][i] = massive[0][n - 1 - i];
         massive[0][n - 1 - i] = ch;
-	    
+
         ch = massive[m - 1][i];
         massive[m - 1][i] = massive[m - 1][n - 1 - i];
         massive[m - 1][n - 1 - i] = ch;
+
     }
 
 
 
     printf("\n   \n");
+
     for ( i = 0; i < m; i++) {
         for (j = 0; j < n; j++){
             printf("%d\t ",massive[i][j]);
         }
         printf("\n");
     }
+
+
     getchar();
-    return 0;
+    
+    
+     /*Part 3*/
+for(j=0;j<n;j++) 
+{
+      printf ("\n Deleting\n\n");
+      int newmas[m-1][n-1]; /*new massive*/
+      int xi=0;
+      int xj=0;
+      for(i=0;i<m-1;i++)
+      {
+         for(j=0;j<n-1;j++)
+         {
+             xi=1;
+             xj=1;
+             newmas[i][j]=massive[i+xi][j+xj];
+             printf("%4d ", newmas[i][j]);
+          }
+          printf("\n");
+      }
 }
+    printf("\n");
+ 
+ /*Part 4*/
+    
+    
+    
+    return 0;
