b870151ea4d9971341546a3a64b80a054018fbc8
Reznikova Varia <31695448+RainbowLamb@users.noreply.github.com>
2017-12-20 23:07:24 +0300

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAXLINES 8
+#define MAXLINES 10
 #define MINLINES 3
-#define MAXCOLUMNS 8
+#define MAXCOLUMNS 10
 #define MINCOLUMNS 3
 #define LEFTBORDER 0
 #define RIGHTBORDER 100
@@ -15,14 +15,16 @@
     /*Part 1 Random matrix*/
 
     int m, n;
-	 /* m - lines; n - columns*/
+	 /* m - lines 
+    n - columns
+    part 1 */
     srand(time(NULL));
     n = rand() % (MAXCOLUMNS-MINCOLUMNS+1) + MINCOLUMNS; 
     m = rand()%(MAXLINES-MINLINES+1) + MINLINES; 
     int massive[m][n];
     int i;
     int j;
-    printf("Creating\n\n");
+    printf("\n Creating\n\n");
     for(i=0; i<m; i++)
     {
         for(j=0; j<n; j++)
@@ -30,7 +32,6 @@
         printf("\n");
     }
     
-	  
     /* part 2 Switch lines*/
     printf ("\n Switching\n");
       for( j = 0; j < n; j++) 
@@ -39,6 +40,7 @@
         massive[0][j] = massive[m - 1][j];
         massive[m - 1][j] = ch;
     }
+
     for ( i = 0; i < n/2; i++)
 	 {
         int ch;
@@ -49,9 +51,11 @@
         ch = massive[m - 1][i];
         massive[m - 1][i] = massive[m - 1][n - 1 - i];
         massive[m - 1][n - 1 - i] = ch;
+
     }
+
     printf("\n   \n");
-	  
+
     for ( i = 0; i < m; i++) 
 	{
         for (j = 0; j < n; j++)
@@ -61,7 +65,6 @@
         printf("\n"); 
     }
 
-	  
      /*Part 3 Deleting*/
         for(j=0;j<n;j++) 
 		{
@@ -80,34 +83,55 @@
         }
             printf("\n");
     }
-}
+
     printf("\n");
    
    
  /*Pqrt 4 New user matrix*/
-   /*m - lines in new matrix; n - colums on new matrix*/
-    printf("Enter new massive size\n");
-    scanf("%d%d", &m, &n);
-    printf ("\n");
-    int new_massive[m][n];
-    if (m>=3 && n>=4);
-    {
-    printf ("\n Massive\n\n");
-        for (i=0; i<n; i++)
-        {
-            for (j=0; j<m; j++)
-            {
-                new_massive[i][j]=rand()%100+1;
-                printf ("%d\t", new_massive[i][j]);
-            }
-            printf("\n");
-        }
-    }
-    if (m<3 && n<4)
-        printf("Error");
-        
-        
-        /*part 5 Multopling */
+    
+    printf ("\n New\n\n");
+    int user_matrix;
+    printf("Enter number of columns");
+    scanf("%d", &user_matrix);
+    int g[n-1][user_matrix];
 
-  return 0;
+    for(i = 0; i < n - 1; i ++)
+    for(j = 0; j < user_matrix; j ++)
+        {
+            g[i][j] = 0;
+        }
+    printf("new array's size = [%d][%d]\n", n - 1, user_matrix);
+    for(i = 0; i < n - 1; i ++)
+        {
+            for(j = 0; j < user_matrix; j ++)
+           {
+               printf("fill in with numbers >");
+               scanf("%d", &g[i][j]);
+            }
+        printf("\n");
+    }
+
+    int end;
+    int sum = 0;
+    printf("\n_________________________________\n");
+
+    for (i=0;i < m-1;i++) 
+    printf ("\n Multiplying\n\n");
+    
+ /*multiply matrixs*/
+    {
+        for (j=0;j < user_matrix;j++)
+        {
+            for (end=0;end < n - 1;end++)
+            {
+                  sum+=del_mas[i][j]*g[end][j];
+            }
+        printf("%d\t",sum);
+        sum = 0;
+    }
+    printf("\n");
+    }
+
+    return 0;
+   }
 }
